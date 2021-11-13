44ea8560eac3de9545daa78c89b3d782f4bc1f9a
benNjogu <kamandemachafi.gmail.com>
2021-11-13 20:30:19 +0300

    Many to Many JPA relationships

--- RecipeApp/src/main/java/com/keytech/RecipeApp/RecipeAppApplication.java
+++ RecipeApp/src/main/java/com/keytech/RecipeApp/RecipeAppApplication.java
@@ -1,9 +1,14 @@
 package com.keytech.RecipeApp;
 
+import javax.persistence.Entity;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.keytech"})
 public class RecipeAppApplication {
 
 	public static void main(String[] args) {
