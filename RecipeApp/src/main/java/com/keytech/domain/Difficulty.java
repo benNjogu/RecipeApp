1abbb7e87f2a4bbd63c3c86a5df48bcdcf5ffc10
benNjogu <kamandemachafi.gmail.com>
2021-11-13 19:37:31 +0300

    JPA Enumerations

---
+++ RecipeApp/src/main/java/com/keytech/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.keytech.domain;
+
+public enum Difficulty {
+	EASY, MODERATE, HARD
+}
--- RecipeApp/src/main/java/com/keytech/domain/Recipe.java
+++ RecipeApp/src/main/java/com/keytech/domain/Recipe.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -17,7 +19,7 @@ public class Recipe {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String description;
 	private Integer prepTime;
 	private Integer cookTime;
@@ -25,18 +27,19 @@ public class Recipe {
 	private String source;
 	private String url;
 	private String direction;
-	// todo add
-	// private Difficulty difficulty;
 
 	@Lob
 	private Byte[] image;
-	
+
+	@Enumerated(value = EnumType.STRING)
+	private Difficulty difficulty;
+
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
-	
+
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
 	private Set<Ingridient> ingridients;
-	
+
 	public Long getId() {
 		return id;
 	}
@@ -124,7 +127,13 @@ public class Recipe {
 	public void setIngridients(Set<Ingridient> ingridients) {
 		this.ingridients = ingridients;
 	}
-	
-	
+
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
 
 }
