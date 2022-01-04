3565f09a0a07c2b78b961c0668b084f6774f8ef9
benNjogu <kamandemachafi.gmail.com>
2022-01-04 23:56:20 +0300

    failed test!

---
+++ RecipeApp/src/test/java/com/keytech/Controllers/IndexControllerTest.java
@@ -0,0 +1,42 @@
+package com.keytech.Controllers;
+
+import static org.hamcrest.CoreMatchers.any;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+import static org.mockito.mockito.*;
+
+import com.keytech.service.RecipeService;
+
+class IndexControllerTest {
+
+	@Mock
+	RecipeService recipeService;
+	
+	@Mock
+	Model model;
+	
+	IndexController indexController;
+	
+	@BeforeEach
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		
+		indexController = new IndexController(recipeService);
+	}
+	
+	@Test
+	void testGetIndexPage() {
+		String viewName = indexController.getIndexPage(model);
+		assertEquals("index", viewName);
+		verify(recipeService, times(1)).getRecipes();
+		verify(model, times(1)).addAttribute(eq("recipes"),anySet());
+	}
+
+}
