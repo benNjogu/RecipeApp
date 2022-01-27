41d0a7153d6f33c1ad7ae76fd6e8e97fdaf42e8e
benNjogu <kamandemachafi.gmail.com>
2022-01-27 19:08:57 +0300

    RecipeServiceImpl test

--- RecipeApp/src/test/java/com/keytech/service/RecipeServiceImplTest.java
+++ RecipeApp/src/test/java/com/keytech/service/RecipeServiceImplTest.java
@@ -1,24 +1,32 @@
 package com.keytech.service;
 
+
+import static org.mockito.Matchers.any;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.keytech.domain.Recipe;
 import com.keytech.repositories.RecipeRepository;
 
+@ExtendWith(MockitoExtension.class)
 class RecipeServiceImplTest {
 
-	RecipeServiceImpl recipeServiceImpl;
+	RecipeServiceImpl recipeService;
 	
 	@Mock
 	RecipeRepository recipeRepository;
@@ -26,7 +34,7 @@ class RecipeServiceImplTest {
 	@BeforeEach
 	public void setup() {
 		MockitoAnnotations.openMocks(this);
-		recipeServiceImpl = new RecipeServiceImpl(recipeRepository);
+		recipeService = new RecipeServiceImpl(recipeRepository);
 	}
 	
 	@Test
@@ -37,10 +45,25 @@ class RecipeServiceImplTest {
 		
 		when(recipeRepository.findAll()).thenReturn(recipeData);
 		
-		Set<Recipe> recipes = recipeServiceImpl.getRecipes();
+		Set<Recipe> recipes = recipeService.getRecipes();
 		
 		assertEquals(recipes.size(), 1);
 		verify(recipeRepository, times(1)).findAll();
+	}
+	
+	@Test
+	void testGetRecipeById() {
+		Recipe recipe = new Recipe();
+		recipe.setId(1L);
+		Optional<Recipe> recipeOptional = Optional.of(recipe);
+		when(recipeRepository.findById(any())).thenReturn(recipeOptional);
+		
+		Recipe returnedRecipe = recipeService.findById(1L);
+		
+		assertNotNull("Null recipe returned", returnedRecipe);
+		verify(recipeRepository, times(1)).findById(any());
+		verify(recipeRepository, never()).findAll();
+		
 	}
 
 }
