1d30cf47b52f587e8010db36a58b47fab3edb53b
benNjogu <kamandemachafi.gmail.com>
2022-01-27 23:15:30 +0300

    Adding test for the model attribute "recipe"

--- RecipeApp/src/test/java/com/keytech/Controllers/RecipeControllerTest.java
+++ RecipeApp/src/test/java/com/keytech/Controllers/RecipeControllerTest.java
@@ -4,6 +4,7 @@ import static org.mockito.Matchers.any;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -45,7 +46,8 @@ class RecipeControllerTest {
 
 		mockMvc.perform(get("/recipe/show/1"))
 				.andExpect(status().isOk())
-				.andExpect(view().name("recipe/show"));
+				.andExpect(view().name("recipe/show"))
+				.andExpect(model().attributeExists("recipe"));
 	}
 
 }
