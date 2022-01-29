c12451cf5a7e3fee43597ee105dbe15a64381b53
benNjogu <kamandemachafi.gmail.com>
2022-01-29 16:49:20 +0300

    Saving and updating recipes

--- RecipeApp/src/main/java/com/keytech/Controllers/RecipeController.java
+++ RecipeApp/src/main/java/com/keytech/Controllers/RecipeController.java
@@ -2,9 +2,13 @@ package com.keytech.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.keytech.commands.RecipeCommand;
 import com.keytech.service.RecipeService;
 
 @Controller
@@ -22,6 +26,21 @@ public class RecipeController {
 		
 		model.addAttribute("recipe", recipeService.findById(new Long(id)));
 		return "recipe/show";
+	}
+	
+	@RequestMapping("recipe/new")
+	public String newRecipe(Model model) {
+		
+		model.addAttribute("recipe", new RecipeCommand());
+		return "recipe/recipeform";
+	}
+	
+	@PostMapping
+	@RequestMapping("recipe")
+	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+		
+		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+		return "redirect:/recipe/show/" + savedCommand.getId();
 	}
 	
 }
