0e3dd6a2f79aaa332f7d096bc7a60e3048a9b04a
benNjogu <kamandemachafi.gmail.com>
2022-02-06 04:28:55 +0300

    Ingredient controller test

---
+++ RecipeApp/src/test/java/com/keytech/Controllers/IngredientControllerTest.java
@@ -0,0 +1,57 @@
+package com.keytech.Controllers;
+
+import static org.mockito.Matchers.any;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.keytech.commands.RecipeCommand;
+import com.keytech.service.RecipeService;
+
+class IngredientControllerTest {
+
+	@Mock
+	RecipeService recipeService;
+	
+	IngredientController controller;
+	
+	MockMvc mockMvc;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		
+		MockitoAnnotations.initMocks(this);
+		controller = new IngredientController(recipeService);
+		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+	}
+
+	@Test
+	void testListIngredients() throws Exception {
+		//given
+		RecipeCommand recipeCommand = new RecipeCommand();
+		when(recipeService.findCommandById(any())).thenReturn(recipeCommand);
+		
+		//when
+		mockMvc.perform(get("/recipe/1/ingredients"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("recipe/ingredient/list"))
+				.andExpect(model().attributeExists("recipe"));
+		
+		//then
+		verify(recipeService, times(1)).findCommandById(any());
+		
+	}
+
+}
