bcd7f1f4cfe074edb21b6db8079c0eed61d6c2ad
benNjogu <kamandemachafi.gmail.com>
2022-02-10 05:54:02 +0300

    UnitOfMeasureService implementation

---
+++ RecipeApp/src/main/java/com/keytech/service/UnitOfMeasureServiceImpl.java
@@ -0,0 +1,31 @@
+package com.keytech.service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import com.keytech.commands.UnitOfMeasureCommand;
+import com.keytech.converters.UnitOfMeasureToUnitOfMeasureCommand;
+import com.keytech.repositories.UnitOfMeasureRepository;
+
+public class UnitOfMeasureServiceImpl implements UnitOfMeasureService {
+
+	private final UnitOfMeasureRepository unitOfMeasureRepository;
+	private final UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand;
+	
+	public UnitOfMeasureServiceImpl(UnitOfMeasureRepository unitOfMeasureRepository,
+			UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureCommand) {
+		super();
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+		this.unitOfMeasureToUnitOfMeasureCommand = unitOfMeasureCommand;
+	}
+
+	@Override
+	public Set<UnitOfMeasureCommand> listAllUOMs() {
+		
+		return StreamSupport.stream(unitOfMeasureRepository.findAll().spliterator(), false)
+							.map(unitOfMeasureToUnitOfMeasureCommand::convert)
+							.collect(Collectors.toSet());
+	}
+
+}
