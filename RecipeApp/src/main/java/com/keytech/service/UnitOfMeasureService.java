0176305249bab24ef4afabe62f65dd45aea2af6b
benNjogu <kamandemachafi.gmail.com>
2022-02-10 05:53:28 +0300

    UnitOfMeasure service

---
+++ RecipeApp/src/main/java/com/keytech/service/UnitOfMeasureService.java
@@ -0,0 +1,11 @@
+package com.keytech.service;
+
+import java.util.Set;
+
+import com.keytech.commands.UnitOfMeasureCommand;
+
+public interface UnitOfMeasureService {
+
+	Set<UnitOfMeasureCommand> listAllUOMs();
+	
+}
