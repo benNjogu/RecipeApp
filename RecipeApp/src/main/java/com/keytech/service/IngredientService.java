5eda34003c648bd7ccab6764c6afac46cc02f21c
benNjogu <kamandemachafi.gmail.com>
2022-02-12 06:12:15 +0300

    Adding delete method

--- RecipeApp/src/main/java/com/keytech/service/IngredientService.java
+++ RecipeApp/src/main/java/com/keytech/service/IngredientService.java
@@ -7,4 +7,6 @@ public interface IngredientService {
 	IngredientCommand findByRecipeIdandIngredientId(Long recipeId, Long ingredientId);
 	
 	IngredientCommand saveIngredientCommand(IngredientCommand command);
+	
+	void deleteById(Long recipeId, Long ingredientId);
 }
