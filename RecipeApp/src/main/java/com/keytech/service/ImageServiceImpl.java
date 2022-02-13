25ceeff69dff1ada2502600d3279ad6e7afe2d62
benNjogu <kamandemachafi.gmail.com>
2022-02-13 22:03:18 +0300

    Adding ImageSaveImplementation

--- RecipeApp/src/main/java/com/keytech/service/ImageServiceImpl.java
+++ RecipeApp/src/main/java/com/keytech/service/ImageServiceImpl.java
@@ -1,7 +1,14 @@
 package com.keytech.service;
 
+import java.io.IOException;
+import java.util.Iterator;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.keytech.domain.Recipe;
+import com.keytech.repositories.RecipeRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,10 +16,36 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class ImageServiceImpl implements ImageService{
 
+	private final RecipeRepository recipeRepository;
+	
+	public ImageServiceImpl(RecipeRepository recipeRepository) {
+		super();
+		this.recipeRepository = recipeRepository;
+	}
+
+	@Transactional
 	@Override
 	public void saveImageFile(Long recipeId, MultipartFile file) {
 		
-		log.debug("Received a file");
+		try {
+			
+			Recipe recipe = recipeRepository.findById(recipeId).get();
+			Byte[] bytesObjects = new Byte[file.getBytes().length];
+			
+			int i = 0;
+			
+			for (Byte b : file.getBytes()) {
+				bytesObjects[i++] = b;
+			}
+			
+			recipe.setImage(bytesObjects);
+			recipeRepository.save(recipe);
+			
+		} catch (IOException e) {
+			//TODO handle better
+			log.debug("An error occured ", e);
+			e.getStackTrace();
+		}
 	}
 
 }
